4f9ff3f626bb015f97e0e071151b18846b9bb93d
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-12-18 16:23:59 +0200

    auth page

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,30 @@
+import 'package:dribbox/core/resources/color%20manager.dart';
+import 'package:dribbox/core/resources/font%20weight%20manager.dart';
+import 'package:dribbox/core/resources/style%20manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/utils/constants.dart';
+import 'features/auth feature/presentation/pages/auth page.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   runApp(const Dribbox());
 }
+
 class Dribbox extends StatelessWidget {
   const Dribbox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.brown,
-        scaffoldBackgroundColor: Colors.white
-      ),
-      home: SplashPage(),
+          primaryColor: Colors.brown, scaffoldBackgroundColor: Colors.white),
+      home: const AuthPage(),
     );
   }
 }
-
-
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(kAuthImage,fit: BoxFit.fill,),),
-          ],
-        ),
-
-
-
-      ),
-    );
-  }
-}
-
-
