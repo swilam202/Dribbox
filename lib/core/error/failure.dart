8a4498833bc3226fee49c460007c95ec7870cd23
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2024-01-03 22:55:36 +0200

    failure class

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,12 @@
+class Failure {
+  final String message;
+
+  const Failure(this.message);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Failure && message == other.message;
+
+  @override
+  int get hashCode => message.hashCode;
+}
