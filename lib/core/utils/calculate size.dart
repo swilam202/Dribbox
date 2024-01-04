817f6b2195353a05ef2a047ceb3a22c70062ee7a
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2024-01-04 15:51:24 +0200

    update size calculator

--- lib/core/utils/calculate size.dart
+++ lib/core/utils/calculate size.dart
@@ -3,14 +3,15 @@ String calculateSize(int bytes) {
 
   if (bytes >= 1099511627776) {
     size = bytes / 1099511627776;
-    return '$size GB';
+    return '${size.toStringAsFixed(1)} GB';
   } else if (bytes >= 1048576) {
     size = bytes / 1048576;
-    return '$size MB';
+    return '${size.toStringAsFixed(1)} MB';
   } else if (bytes >= 1024) {
     size = bytes / 1024;
-    return '$size KB';
+    return '${size.toStringAsFixed(1)} KB';
   } else {
-    return '$bytes Byte';
+    size = bytes.toDouble();
+    return '${size.toStringAsFixed(1)} Byte';
   }
 }
