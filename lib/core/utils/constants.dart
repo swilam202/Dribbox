eca2bcf240a4c4aabe38f681782581e6154d8863
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2024-01-06 16:12:22 +0200

    custom snack bar

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,2 +1,3 @@
 
 const String kAuthImage = 'assets/images/auth icon.png';
+const Duration kDuration = Duration(milliseconds: 2000);
---
+++ lib/core/widgets/custom snack bar.dart
@@ -0,0 +1,20 @@
+import 'package:dribbox/core/resources/color%20manager.dart';
+import 'package:dribbox/core/utils/constants.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+customSnackBar(String title, String message) {
+  return Get.snackbar(
+    title,
+    message,
+    backgroundColor: ColorManager.blueColor,
+    icon: const Icon(Icons.warning, color: ColorManager.redColor),
+    duration: kDuration,
+    isDismissible: true,
+    overlayBlur: 3,
+    snackPosition: SnackPosition.BOTTOM,
+    animationDuration: kDuration,
+    forwardAnimationCurve: Curves.fastEaseInToSlowEaseOut,
+    margin: const EdgeInsets.all(20),
+  );
+}
